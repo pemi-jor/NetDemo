f583f7e6a03030fe450d5723c4cd9db545c22a59
pemi-jor <mupka.espl@gmail.com>
2020-06-16 21:56:18 +0300

    update

--- NetDemo/Models/DemoBaseContext.cs
+++ NetDemo/Models/DemoBaseContext.cs
@@ -24,7 +24,7 @@ namespace NetDemo.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=DemoBase;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=DataBase;Trusted_Connection=True;");
             }
         }
 
